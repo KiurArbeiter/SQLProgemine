0dbd6c1f4e8228467121702c4b10296ba05e4683
Mathias <127192950+uleksin@users.noreply.github.com>
2023-03-07 11:10:09 +0200

    Tund 07.03.2023

---
+++ SQL.sql
@@ -0,0 +1,147 @@
+-- komm
+-- teeb andmbeaasi ehk db
+create database TARpe22
+
+-- kustutab db
+drop database TARpe22
+
+-- tabeli loomine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar(10) not null
+)
+
+---- andmete sisestamine
+insert into Gender (Id, Gender)
+values (2, 'Male')
+insert into Gender (Id, Gender)
+values (1, 'Female')
+insert into Gender (Id, Gender)
+values (3, 'Unknown')
+
+--- sama Id väärtusega rida ei saa sisestada
+select * from Gender
+
+--- teeme uue tabeli
+create table Person
+(
+	Id int not null primary key,
+	Name nvarchar(30),
+	Email nvarchar(30),
+	GenderId int
+)
+
+--- vaatame Person tabeli sisu
+select * from Person
+
+--- andmete sisestamine
+insert into Person (Id, Name, Email, GenderId)
+values (1, 'Superman', 'superman@mail.com', 2)
+insert into Person (Id, Name, Email, GenderId)
+values (2, 'Wonderwoman', 'ww@mail.com', 1)
+insert into Person (Id, Name, Email, GenderId)
+values (3, 'Batman', 'bm@mail.com', 2)
+insert into Person (Id, Name, Email, GenderId)
+values (4, 'Aquaman', 'am@mail.com', 2)
+insert into Person (Id, Name, Email, GenderId)
+values (5, 'Catwoman', 'cw@mail.com', 1)
+insert into Person (Id, Name, Email, GenderId)
+values (6, 'Antman', 'ant"ant.com', 2)
+insert into Person (Id, Name, Email, GenderId)
+values (8, NULL, NULL, 2)
+
+select * from Person
+
+-- võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderID_FK
+foreign key (GenderId) references Gender(Id)
+
+--- kui sisestad uue rea andmeid ja ei ole sisestatud GenderId all väärtust, siis
+--- see automaatselt sisestab tabelisse väärtuse 3 ja selleks on Unknown
+alter table Person
+add constraint DF_Persons_GenderId
+default 3 for GenderId
+
+insert into Person (Id, Name, Email)
+values (9, 'Ironman', 'i@i.com')
+
+select * from Person
+
+-- piirangu maha võtmine
+alter table Person
+drop constraint DF_Persons_GenderId
+
+--- lisame uue veeru
+alter table Person
+add Age nvarchar(10)
+
+--- lisame vanuse piirangu sisestamisel
+--- ei saa lisada suuremat väärtust kui 801
+alter table Person
+add constraint CK_Person_Age check (Age > 0 and Age < 801)
+
+-- rea kustutamine
+-- kui paned vale id, siis ei muuda midagi
+delete from Person where Id = 9
+
+select * from Person
+
+-- kuidas uuendada andmeid tabelis
+update Person
+set Age = 50
+where Id = 1
+
+-- lisame juurde uue veeru
+alter table Person
+add City nvarchar(50)
+
+-- kõik, kes elavad gothami linnas
+select * from Person where City = 'Gotham'
+-- kõik, kes ei ela Gothamis
+select * from Person where City != 'Gotham'
+-- teine variant
+select * from Person where not City = 'Gotham'
+-- kolmas variant
+select * from Person where City <> 'Gotham'
+
+-- naitab teatud vanusega inimes
+select * from Person where Age = 800 or Age = 35 or Age = 27
+select * from Person where Age in (800, 35, 27)
+
+-- naitab teatud vanusevahemikus olevaid inimesi
+select * from Person where Age between 20 and 35
+
+-- wildcard ehk naitab koik g-tahega linnad
+select * from Person where City like 'g%'
+-- naitab koik emailid, milles on @ mark
+select * from Person where Email like '%@%'
+
+--- näitab kõiki, kellel ei ole @-märki emailis
+select * from Person where Email not like '%@%'
+
+--- naitab kellel on emailis ees ja peale @-märki
+select * from Person where Email like '_@_.com'
+
+-- koik, kellel ei ole nimes esimene taht W, A, C
+select * from Person where Name like '[^WAC]%'
+
+--- kes elavad Gothamis ja New Yorkis
+select * from Person where (City= 'Gotham' or City = 'New York')
+
+-- koik kes elavad Gothamis ja New Yorkis ning
+-- alla 30 eluaastat
+select * from Person where 
+(City= 'Gotham' or City = 'New York')
+and Age <= 30
+
+--- kuvab tahestikulises jarjekorras inimesi ja vütab aluseks nime
+select * from Person order by Name
+-- kuvab ´vastupidises jarjekorras inimesi ja vütb aluseks nime
+select * from Person order by Name desc
+
+-- votab kolm esimest rida
+select TOP 3 * from Person
+
+
+--- 2 tund jahjah
